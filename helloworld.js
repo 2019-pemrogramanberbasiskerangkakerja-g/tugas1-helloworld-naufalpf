4885bc214a4596d467666d3181d78d91b99f793a
Naufal Pranasetyo <naufalpf@gmail.com>
2019-02-04 15:23:33 +0700

    tugas 1

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var port = 3000
+var port = 2000;
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(port, function() {
-    console.log("run")
-})
+    console.log('Buka browser dan ketik ke alamat localhost:2000')
+});
